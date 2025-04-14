a77dde8056cb6c20601871f515aa540e5cb3bfa6
antonBlagodarnyy <antonbgi98@gmail.com>
2025-04-14 11:35:11 +0200

    importacion del servicio que recibe el texto y llamada a la funcion

--- frontend/index.php
+++ frontend/index.php
@@ -1,3 +1,13 @@
+<?php
+require_once(__DIR__.'/../backend/service/funciones.php');
+?>
+
+<?php
+    if($_SERVER['REQUEST_METHOD' == "POST"]){
+        //TODO implementar la funcion en el backend
+        //recogerTexto($_POST['texto']);
+    }
+    ?>
 <!DOCTYPE html>
 <html lang="en">
 
@@ -27,11 +37,12 @@
         <h1>Proyecto de despliegue</h1>
     </div>
     <div class="container">
-    <form action="<?= __DIR__ ?>/../backend/service/contarPalabras.php">
-        <textarea name="palabras" id="palabras"></textarea>
+    <form method="POST">
+        <textarea name="palabras" id="palabras" name="texto"></textarea>
         <button type="submit">Contar palabras</button>
     </form>
     </div>
+    
 </body>
 
 </html>
