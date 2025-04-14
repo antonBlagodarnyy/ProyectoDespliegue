c63f0d8b44a704ea6b93bba4e3f8b578a0221703
antonBlagodarnyy <antonbgi98@gmail.com>
2025-04-14 11:25:10 +0200

    formulario de introduccion de texto

--- frontend/index.php
+++ frontend/index.php
@@ -4,14 +4,34 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>Proyecto de despliegue</title>
+    <style>
+        body{
+            display: flex;
+            flex-direction: column;
+            align-items: center;
+        }
+        textarea{
+            width: 50vh;
+            height: 50vh;
+        }
+        form{
+            display: flex;
+            flex-direction: column;
+        }
+    </style>
 </head>
 
 <body>
+    <div class="header">
+        <h1>Proyecto de despliegue</h1>
+    </div>
+    <div class="container">
     <form action="<?= __DIR__ ?>/../backend/service/contarPalabras.php">
         <textarea name="palabras" id="palabras"></textarea>
-        <button type="submit"></button>
+        <button type="submit">Contar palabras</button>
     </form>
+    </div>
 </body>
 
 </html>
