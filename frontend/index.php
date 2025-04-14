9e1f0018fc4dc88e4d74457753367552edc07525
antonBlagodarnyy <antonbgi98@gmail.com>
2025-04-14 14:08:29 +0200

    implementacion inicial

--- frontend/index.php
+++ frontend/index.php
@@ -1,13 +1,14 @@
 <?php
-require_once(__DIR__.'/../backend/service/funciones.php');
+require_once(__DIR__ . '/../backend/service/funciones.php');
 ?>
 
 <?php
-    if($_SERVER['REQUEST_METHOD' == "POST"]){
-        //TODO implementar la funcion en el backend
-        recogerTexto($_POST['texto']);
-    }
-    ?>
+$palabras = [];
+
+if ($_SERVER['REQUEST_METHOD'] == "POST") {
+    $palabras = ordenarPalabras(contarPalabras(recogerTexto($_POST['palabras'])));
+}
+?>
 <!DOCTYPE html>
 <html lang="en">
 
@@ -16,16 +17,18 @@ require_once(__DIR__.'/../backend/service/funciones.php');
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Proyecto de despliegue</title>
     <style>
-        body{
+        body {
             display: flex;
             flex-direction: column;
             align-items: center;
         }
-        textarea{
+
+        textarea {
             width: 50vh;
             height: 50vh;
         }
-        form{
+
+        form {
             display: flex;
             flex-direction: column;
         }
@@ -37,12 +40,18 @@ require_once(__DIR__.'/../backend/service/funciones.php');
         <h1>Proyecto de despliegue</h1>
     </div>
     <div class="container">
-    <form method="POST">
-        <textarea name="palabras" id="palabras" name="texto"></textarea>
-        <button type="submit">Contar palabras</button>
-    </form>
+        <form method="POST">
+            <textarea name="palabras" id="palabras" name="texto"></textarea>
+            <button type="submit">Contar palabras</button>
+        </form>
     </div>
-    
+    <?php if (count($palabras)>0): ?>
+        <ul>
+            <?php foreach ($palabras as $palabra => $ocurrencia): ?>
+                <li><?= $palabra ?> : <?= $ocurrencia ?></li>
+            <?php endforeach; ?>
+        </ul>
+    <?php endif ?>
 </body>
 
 </html>
