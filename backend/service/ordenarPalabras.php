e05e8d01319f75ce0d3165acc426f16004bf9449
antonBlagodarnyy <antonbgi98@gmail.com>
2025-04-21 10:48:32 +0200

    fix de ordenar palabras

--- backend/service/ordenarPalabras.php
+++ backend/service/ordenarPalabras.php
@@ -3,10 +3,29 @@
 function ordenarPalabras(array $palabras)
 {
     //Podria utilizar sort()
-    $palabrasOrdenadas = [];
 
-    foreach ($palabras as $key => $palabra) {
-        $palabrasOrdenadas[$key] = max($palabras);
+    $claves = array_keys($palabras);
+    $n = count($claves);
+
+   
+    for ($i = 0; $i < $n - 1; $i++) {
+        for ($j = 0; $j < $n - $i - 1; $j++) {
+            $claveActual = $claves[$j];
+            $claveSiguiente = $claves[$j + 1];
+
+            if ($palabras[$claveActual] < $palabras[$claveSiguiente]) {
+
+                $temp = $claves[$j];
+                $claves[$j] = $claves[$j + 1];
+                $claves[$j + 1] = $temp;
+            }
+        }
     }
-    return $palabrasOrdenadas;
+
+    $ordenado = [];
+    foreach ($claves as $clave) {
+        $ordenado[$clave] = $palabras[$clave];
+    }
+
+    return $ordenado;
 }
