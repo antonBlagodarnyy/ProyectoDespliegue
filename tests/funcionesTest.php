2921866b214539f81a58845ec0f132aa2613370e
antonBlagodarnyy <antonbgi98@gmail.com>
2025-05-21 11:54:32 +0200

    refactorizacion de los tests. Falta el main con funciones mock

--- tests/funcionesTest.php
+++ tests/funcionesTest.php
@@ -1,4 +1,5 @@
 <?php
+
 use PHPUnit\Framework\TestCase;
 
 require_once __DIR__ . '/../backend/service/funciones.php';
@@ -6,80 +7,64 @@ require_once __DIR__ . '/../backend/service/funciones.php';
 
 class FuncionesTest extends TestCase
 {
-    public function testRecogerTextoEliminaDeterminantes()
+    public function test_eliminaDeterminantes()
     {
-        $texto = "El perro corre por el parque y la dueña ve un ¿Camión con una gata que la mira?.  árbol esta está recorrió la gata
-        y la gata se quedo en la copa #";//La cadena que entraria desde el formulario en el frontend
-        $resultado = recogerTexto($texto);
+        //Funcion mock para testear eliminarDeterminantes
+        $texto = filtrarPalabras("El perro corre por el parque y la dueña ve un ¿Camión con una gata que la mira?.  árbol esta está recorrió la gata
+        y la gata se quedo en la copa #"); //La cadena que entraria desde el formulario en el frontend
+
+        $resultado = eliminarDeterminantes($texto);
 
         //Los resultados que se espera que devuelva la funcion dentro del array palabras
         $this->assertContains('perro', $resultado);
-        $this->assertContains('corre', $resultado);
-        $this->assertContains('parque', $resultado);
-        $this->assertContains('dueña', $resultado);
-        $this->assertContains('gata', $resultado);
-        $this->assertContains('mira', $resultado);
-        
-
-        $this->assertContains('camión', $resultado);//Las letras en mayuscula en el texto, se convierten
-        //a minuscula cuando pasan por la funcion, por eso aqui espero que camion se pase a minuscula
-        //cuando la funcion devuelva el resultado
+        $this->assertContains('Camión', $resultado);
         $this->assertContains('árbol', $resultado);
-        
         $this->assertContains('recorrió', $resultado);
 
         //Los resultados que se espera que la funcion excluya, ya sean porque son palabras determinantes
         // o porque son caracteres especiales
 
-        $this->assertNotContains('el', $resultado);
-         $this->assertNotContains('?', $resultado);
-          $this->assertNotContains('¿', $resultado);
-        $this->assertNotContains('la', $resultado);
-         $this->assertNotContains('una', $resultado);
-         $this->assertNotContains('un', $resultado);
-         $this->assertNotContains('en', $resultado);
-         $this->assertNotContains('se', $resultado);
-          $this->assertNotContains('con', $resultado);
-       // $this->assertNotContains('lo', $resultado);
-        $this->assertNotContains('y', $resultado);
-
-        $this->assertNotContains('está', $resultado);
-        $this->assertNotContains('esta', $resultado);
+        $this->assertNotContains('el',  $resultado);
+        $this->assertNotContains('?',  $resultado);
+        $this->assertNotContains('¿',  $resultado);
+        $this->assertNotContains('la',  $resultado);
+        $this->assertNotContains('una',  $resultado);
+        $this->assertNotContains('un',  $resultado);
+        $this->assertNotContains('en',  $resultado);
+        $this->assertNotContains('se',  $resultado);
+        $this->assertNotContains('con',  $resultado);
+        $this->assertNotContains('y',  $resultado);
+        $this->assertNotContains('está',  $resultado);
+        $this->assertNotContains('esta',  $resultado);
         $this->assertNotContains('#', $resultado);
     }
 
-     public function testRecogerTextoConTextoVacio()
+    public function test_filtrarPalabrasTextoVacio()
     {
-        $resultado = recogerTexto("");
+        $resultado = filtrarPalabras("");
         $this->assertIsArray($resultado);
         $this->assertEmpty($resultado);
     }
 
-     public function testRecogerTextoSoloCaracteresEspeciales()
+    public function test_filtrarPalabrasSoloCaracteresEspeciales()
     {
         $texto = "#$%&?¿!¡";
-        $resultado = recogerTexto($texto);
+        $resultado = filtrarPalabras($texto);
+
         $this->assertEmpty($resultado);
     }
 
-     public function testRecogerTextoConTildes()
+    public function test_filtrarPalabrasTildes()
     {
         $texto = "camión árbol acción";
-        $resultado = recogerTexto($texto);
-        $this->assertContains('camión', $resultado);
-        $this->assertContains('árbol', $resultado);
+        $resultado = filtrarPalabras($texto);
+        $this->assertContains('camión',  $resultado);
+        $this->assertContains('árbol',  $resultado);
         $this->assertContains('acción', $resultado);
     }
 
-    public function testRecogerTextoMayusculasMinusculas()
-    {
-        $texto = "PERRO gato GATO";
-        $resultado = recogerTexto($texto);
-        $this->assertContains('perro', $resultado);
-        $this->assertContains('gato', $resultado);
-    }
 
-    public function testContarPalabrasCuentaCorrectamente()
+    public function test_contarPalabras()
     {
         $palabras = ['perro', 'gata', 'perro', 'mira'];
         $resultado = contarPalabras($palabras);
@@ -89,24 +74,15 @@ class FuncionesTest extends TestCase
         $this->assertEquals(1, $resultado['mira']);
     }
 
-     public function testContarPalabrasVacio()
+    public function test_contarPalabrasVacio()
     {
         $resultado = contarPalabras([]);
         $this->assertIsArray($resultado);
         $this->assertEmpty($resultado);
     }
 
-    public function testContarPalabrasRepetidasDesorden()
-    {
-        $palabras = ['gato', 'perro', 'gato', 'gato'];
-        $resultado = contarPalabras($palabras);
 
-        $this->assertEquals(3, $resultado['gato']);
-        $this->assertEquals(1, $resultado['perro']);
-    }
-
-
-    public function testOrdenarPalabrasOrdenaDescendente()
+    public function test_ordenarPalabras()
     {
         $palabrasContadas = [
             'mira' => 1,
@@ -125,27 +101,37 @@ class FuncionesTest extends TestCase
         $this->assertEquals($esperado, $ordenado);
     }
 
-    public function testOrdenarPalabrasVacio()
+    public function test_sanearTextoPalabrasMayusculas()
     {
-        $resultado = ordenarPalabras([]);
-        $this->assertIsArray($resultado);
-        $this->assertEmpty($resultado);
+        $texto = "PERRO gato GATO";
+        $resultado = sanearTexto($texto);
+        $this->assertStringContainsString('perro',  $resultado);
+        $this->assertStringContainsString('gato', $resultado);
     }
 
-    public function testOrdenarPalabrasConFrecuenciasIguales()
+    public function test_sanearTextoMinusculas()
     {
-        $palabrasContadas = [
-            'gato' => 2,
-            'perro' => 2,
-            'loro' => 2
-        ];
-
-        $resultado = ordenarPalabras($palabrasContadas);
-
-        $this->assertEqualsCanonicalizing([
-            'gato' => 2,
-            'perro' => 2,
-            'loro' => 2
-        ], $resultado);
+        $input = "  HÓLa  ";
+        $expected = "hóla";
+        $this->assertSame($expected, sanearTexto($input));
     }
+
+    public function test_sanearTextoEncoding()
+    {
+        $text = mb_convert_encoding("Camión", 'unicode');
+
+        $encoding = mb_detect_encoding(sanearTexto($text), 'UTF-8', true);
+        $this->assertEquals('UTF-8', $encoding);
+    }
+    public function test_main()
+    {
+        $texto = "El perro corre por el parque y la dueña ve un ¿Camión con una gata que la mira?.  árbol esta está recorrió la gata
+        y la gata se quedo en la copa #";
+
+        $resultado = main($texto);
+
+        $this->assertEquals(["gata" => 3, "perro" => 1, "corre" => 1, "parque" => 1, "dueña" => 1, "ve" => 1, "camión" => 1, "mira" => 1, "árbol" => 1, "recorrió" => 1, "quedo" => 1, "copa" => 1], $resultado);
+    }
+
+ 
 }
